078b8758472ea1000314a18077680f25d3e7de5c
edumaia23 <sonoswell@hotmail.com>
2022-11-07 14:45:33 -0300

    dados voos em geral

---
+++ src/database/dados-aeroportos.sql
@@ -0,0 +1,10 @@
+DELETE FROM aeroportos WHERE id>=0;
+    
+  INSERT INTO aeroportos
+		(id, cidade, sigla, nome)
+	VALUES
+		(1, 'Salvador', 'SSA', 'Luis Eduardo Magalhães'),
+        (2, 'Belo Horizonte', 'CNF', 'Confins'),
+        (3, 'Rio de Janeiro', 'GIG', 'Galeão'),
+        (4, 'Guarulhos', 'GRU', 'Cumbica'),
+        (5, 'São Paulo', 'CNH', 'Congonhas');
---
+++ src/database/dados-companhias.sql
@@ -0,0 +1,7 @@
+DELETE FROM companhias WHERE id>=0;
+    
+  INSERT INTO companhias
+		(id, nome)
+	VALUES
+		(1, 'azul'),
+        (2, 'TAM')';
