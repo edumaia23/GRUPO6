0bd714227508337955e3844a793a0a64281c4ac1
edumaia23 <sonoswell@hotmail.com>
2022-12-13 09:17:52 -0300

    Aeroportos

--- src/controllers/passagensController.js
+++ src/controllers/passagensController.js
@@ -1,30 +1,41 @@
 // o controller receberá a solicitação do roteador e encaminhará a requisição para a função determinada, rederizando a view correspondente
 
-const {Voos,Passagem, sequelize} = require('../../models');
+const { Voos, Passagem, sequelize } = require('../../models');
 const passagensController = {  // primeiro passo é criar a variavel e depois criar a função de acordo com cada página do site
 
-    index: (req, res) => {
-        res.render('index.ejs')    
-    },
+  //escrever a consulta q busca os aeroportos
+  //executar a consulta
+  //enviar o resultado junto com view
+
+  index: async (req, res) => {
+    var sql = 'select id, cidade, nome, sigla FROM aeroportos'
+    let aeroportos = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT });
+    res.render('index.ejs',{aeroportos})
+  },
 
 
-    busca: async (req, res) => {
-       const {origem, destino, ida, volta, classe} = req.query
-       var sql=`select passagens.id, passagens.preco, voos_id, data_hora_partida, data_hora_chegada  FROM PASSAGENS inner JOIN VOOS ON PASSAGENS.VOOS_ID = VOOS.ID
+  busca: async (req, res) => {
+    const { origem, destino, ida, volta, classe } = req.query
+    var sql = `select passagens.id, passagens.preco, voos_id, data_hora_partida, data_hora_chegada  FROM PASSAGENS inner JOIN VOOS ON PASSAGENS.VOOS_ID = VOOS.ID
        WHERE AEROPORTOS_ORIGEM_ID = 1 AND AEROPORTOS_DESTINO_ID = 2 AND date(data_hora_partida) = "2022-11-07";`;
-       let resultado = await sequelize.query(sql,{ type: sequelize.QueryTypes.SELECT});
-       console.log(resultado);
+    let resultado = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT });
+    console.log(resultado);
 
-        res.render('passagens.ejs', {resultado})
-      
-        
-        //res.send(passagens)
+    res.render('passagens.ejs', { resultado })
 
-    },
+
+    //res.send(passagens)
+
+  },
 
 
 
 }
+//const calendario = async () => {}
+
+//const resultado = Math.floor(Math.random() * 2500) + 300;
+//console.log('resultado', resultado);
+
 
 module.exports = passagensController; // exporta o arquivo viagens controle
 
